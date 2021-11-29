1d073a388c135bceebdd8f7dbe0f458332c36a1f
vamsi-krishna-kodimela <vamsikrishnakdml@gmail.com>
2021-11-29 23:41:33 +0530

    Simple Implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState("");
+  const addTask = () => {
+    setTask("");
+    setTasks([task, ...tasks]);
+  };
+
+  const removeTask = (ind) => {
+    tasks.splice(ind, 1);
+    setTasks([...tasks]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h1>TODO App</h1>
+      <div className="create-block">
+        <input
+          type="text"
+          value={task}
+          onChange={(event) => setTask(event.target.value)}
+          placeholder="Task"
+        />
+        <button className="btn task-btn flex-auto" onClick={addTask}>
+          Add Task
+        </button>
+      </div>
+      <div className="tasks-list">
+        {tasks.map((tsk, ind) => (
+          <div className="task" key={ind}>
+            <p>{tsk}</p>
+            <button onClick={() => removeTask(ind)}>X</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
